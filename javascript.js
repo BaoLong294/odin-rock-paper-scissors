943197e1baacc91378f7a726bbf0ff68c962b655
Nguyễn Bảo Long <longbao2904@gmail.com>
2025-08-12 15:03:47 +0000

    Refactor game flow and finalize 5-round logic
    - Moved humanScore and computerScore inside playGame()
    - Moved playRound() function inside playGame() for encapsulation
    - Updated getComputerChoice() to use 1/3 and 2/3 thresholds
    - Added final score output and winner announcement after 5 rounds
    - Cleaned up comments and fixed typo "Covert" to "Convert"

--- javascript.js
+++ javascript.js
@@ -3,12 +3,12 @@ function getComputerChoice() {
   // Create variable to store random number
   const randomNumber = Math.random();
 
-  // IF randomNumber < 0.33 return rock
-  if (randomNumber < 0.33) {
+  // IF randomNumber < 1/3 return rock
+  if (randomNumber < 1 / 3) {
     return "rock";
   }
-  // ELSE IF randomNumber < 0.66 return paper
-  else if (randomNumber < 0.66) {
+  // ELSE IF randomNumber < 2/3 return paper
+  else if (randomNumber < 2 / 3) {
     return "paper";
   }
   // ELSE return scissors
@@ -21,73 +21,99 @@ function getComputerChoice() {
 function getHumanChoice() {
   // Displays a prompt asking the player to enter "rock", "paper" or "scissors"
   let input = prompt("Choose rock, paper or scissors: ");
-  // Covert input to lower case to make humanChoice case-insensitive
+  // Convert input to lower case to make humanChoice case-insensitive
   input = input.toLowerCase();
 
   return input;
 }
 
-// Create variable to store human and computer score
-let humanScore = 0;
-let computerScore = 0;
+// Create playGame function to include playRound function and 2 score variables
+function playGame() {
+  // Create variable to store human and computer score
+  let humanScore = 0;
+  let computerScore = 0;
 
-// Create variable store human choice
-const humanSelection = getHumanChoice();
-// Create variable store computer choice
-const computerSelection = getComputerChoice();
+  // Create function receive human choice and computer choice as parameter
+  function playRound(humanChoice, computerChoice) {
+    // Make humanChoice case-insensitive so that players can input “rock”, “ROCK”, “RocK”, or other variations
+    humanChoice = humanChoice.toLowerCase();
+    // IF human choice and computer choice are the same print "Tied! Both chose humanChoice"
+    if (humanChoice === computerChoice) {
+      console.log(`Tied! Both chose ${humanChoice}`);
+      return "tie";
+    }
+    // ELSE IF human choice is rock
+    else if (humanChoice === "rock") {
+      // IF computer choice is paper increases computer's score by 1 and print "You lose! Paper beats Rock"
+      if (computerChoice === "paper") {
+        ++computerScore;
+        console.log("You lose! Paper beats Rock");
+        return "lose";
+      }
+      // ELSE increases human's score by 1 and print "You win! Rock beats Scissors"
+      else {
+        ++humanScore;
+        console.log("You win! Rock beats Scissors");
+        return "win";
+      }
+    }
+    // ELSE IF human choice is paper
+    else if (humanChoice === "paper") {
+      // IF computer choice is scissors increases computer's score by 1 and print "You lose! Scissors beat Paper"
+      if (computerChoice === "scissors") {
+        ++computerScore;
+        console.log("You lose! Scissors beat Paper");
+        return "lose";
+      }
+      // ELSE increases human's score by 1 and print "You win! Paper beats Rock"
+      else {
+        ++humanScore;
+        console.log("You win! Paper beats Rock");
+        return "win";
+      }
+    }
+    // ELSE IF human choice is scissors
+    else if (humanChoice === "scissors") {
+      // IF computer choice is rock increases computer's score by 1 and print "You lose! Rock beats Scissors"
+      if (computerChoice === "rock") {
+        ++computerScore;
+        console.log("You lose! Rock beats Scissors");
+        return "lose";
+      }
+      // ELSE return increases human's score by 1 and print "You win! Scissor beat Paper"
+      else {
+        ++humanScore;
+        console.log("You win! Scissors beat Paper");
+        return "win";
+      }
+    }
+  }
 
-// Create function receive human choice and computer choice as parameter
-function playRound(humanChoice, computerChoice) {
-  // Make humanChoice case-insensitive so that players can input “rock”, “ROCK”, “RocK”, or other variations
-  humanChoice = humanChoice.toLowerCase();
-  // IF human choice and computer choice are the same print "Tied! Both chose humanChoice"
-  if (humanChoice === computerChoice) {
-    console.log(`Tied! Both chose ${humanChoice}`);
+  // Call playRound 5 times
+  playRound(getHumanChoice(), getComputerChoice());
+  playRound(getHumanChoice(), getComputerChoice());
+  playRound(getHumanChoice(), getComputerChoice());
+  playRound(getHumanChoice(), getComputerChoice());
+  playRound(getHumanChoice(), getComputerChoice());
+
+  // Print the winner of the game base on the final score
+  if (humanScore === computerScore) {
+    console.log(
+      `Final score - You: ${humanScore} | Computer: ${computerScore}\nYou and computer are tied!`
+    );
     return "tie";
-  }
-  // ELSE IF human choice is rock
-  else if (humanChoice === "rock") {
-    // IF computer choice is paper increases computer's score by 1 and print "You lose! Paper beats Rock"
-    if (computerChoice === "paper") {
-      ++computerScore;
-      console.log("You lose! Paper beats Rock");
-      return "lose";
-    }
-    // ELSE increases human's score by 1 and print "You win! Rock beats Scissors"
-    else {
-      ++humanScore;
-      console.log("You win! Rock beats Scissors");
-      return "win";
-    }
-  }
-  // ELSE IF human choice is paper
-  else if (humanChoice === "paper") {
-    // IF computer choice is scissors increases computer's score by 1 and print "You lose! Scissors beat Paper"
-    if (computerChoice === "scissors") {
-      ++computerScore;
-      console.log("You lose! Scissors beat Paper");
-      return "lose";
-    }
-    // ELSE increases human's score by 1 and print "You win! Paper beats Rock"
-    else {
-      ++humanScore;
-      console.log("You win! Paper beats Rock");
-      return "win";
-    }
-  }
-  // ELSE IF human choice is scissors
-  else if (humanChoice === "scissors") {
-    // IF computer choice is rock increases computer's score by 1 and print "You lose! Rock beats Scissors"
-    if (computerChoice === "rock") {
-      ++computerScore;
-      console.log("You lose! Rock beats Scissors");
-      return "lose";
-    }
-    // ELSE return increases human's score by 1 and print "You win! Scissor beat Paper"
-    else {
-      ++humanScore;
-      console.log("You win! Scissors beat Paper");
-      return "win";
-    }
+  } else if (humanScore > computerScore) {
+    console.log(
+      `Final score - You: ${humanScore} | Computer: ${computerScore}\nCongratulation! You won!`
+    );
+    return "win";
+  } else {
+    console.log(
+      `Final score - You: ${humanScore} | Computer: ${computerScore}\nUnlucky! You lost!`
+    );
+    return "lose";
   }
 }
+
+// Launch the game
+playGame();

00840cf78b39d8e939a8c9a1bfdaaa63cded4928
Nguyễn Bảo Long <longbao2904@gmail.com>
2025-08-12 13:03:30 +0000

    feat: add getComputerChoice function random picker
    - Create function to get random computer choice, return "rock", "paper"
    or "scissors" using Math.random method

--- javascript.js
+++ javascript.js
@@ -1,1 +1,18 @@
-console.log("Hello");
+// Get random computer choice
+function getComputerChoice() {
+  // Create variable to store random number
+  const randomNumber = Math.random();
+
+  // IF randomNumber < 0.33 return rock
+  if (randomNumber < 0.33) {
+    return "rock";
+  }
+  // ELSE IF randomNumber < 0.66 return paper
+  else if (randomNumber < 0.66) {
+    return "paper";
+  }
+  // ELSE return scissors
+  else {
+    return "scissors";
+  }
+}
